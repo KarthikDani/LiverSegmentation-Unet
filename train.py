6df6ee5ee048d2c606cb3a2a0f1f6b61b757c236
Karthik Dani <47054117+KarthikDani@users.noreply.github.com>
2024-01-23 23:51:17 +0530

    codes and output added

---
+++ train.py
@@ -0,0 +1,29 @@
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+from monai.losses import DiceLoss
+
+import torch
+from preprocess import prepare
+from utilities import train
+
+
+data_dir = '/Users/karthik/liver_segmentation/Data'
+model_dir = '/Users/karthik/liver_segmentation/result'
+data_in = prepare(data_dir, cache=True)
+
+device = torch.device("cpu")
+model = UNet(
+    spatial_dims=3,
+    in_channels=1,
+    out_channels=2,
+    channels=(16, 32, 64, 128, 256),
+    strides=(2, 2, 2, 2),
+    num_res_units=2,
+    norm=Norm.BATCH,
+).to(device)
+
+loss_function = DiceLoss(to_onehot_y=True, sigmoid=True, squared_pred=True)
+optimizer = torch.optim.Adam(model.parameters(), 1e-5, weight_decay=1e-5, amsgrad=True)
+
+if __name__ == '__main__':
+    train(model, data_in, loss_function, optimizer, 50, model_dir)
